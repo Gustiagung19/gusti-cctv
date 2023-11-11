e62205c63c09ad0021642a4c8b30bb0fc4571bb4
Gusti Agung <75777108+Gustiagung19@users.noreply.github.com>
2023-11-11 22:59:49 +0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- [[ FX Information ]] --
+fx_version 'adamant'
+lua54 'yes'
+game 'gta5'
+
+-- [[ Resource Information ]] --
+name 'gusti-cctv'
+author 'Gusti Agung'
+version '1.0.0'
+description 'The gusti-cctv script is a script that makes it easy for players / jobs to control the cctv camera in-game'
